96331951717e58a91d4adddcd86784bda7ed586d
Mahdi_new <mmf1344py@gmail.com>
2025-07-13 15:19:31 +0330

    fix some breadcrumb navigation bug.

--- resources/views/web/pages/about.blade.php
+++ resources/views/web/pages/about.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="p-about">
+        @php
+            $base = \App\Models\Base::find(session('base_id'));
+        @endphp
         <section class="page-title">
             <div class="container">
                 <div class="row">
@@ -11,7 +14,11 @@
                             <h2 class="title">{{ trans('page.pages.about_us') }}</h2>
 
                             <ul class="breacrumd">
-                                <li><a href="/">{{ trans('home.header.home') }} </a></li>
+                                @if($base)
+                                    <li><a href="{{ route('home-by-base', ['locale' => app()->getLocale(), 'base' => $base?->slug]) }}">{{ trans('home.header.home') }} </a></li>
+                                @else
+                                    <li><a href="/">{{ trans('home.header.home') }} </a></li>
+                                @endif
                                 <li>/</li>
                                 <li>{{ trans('page.pages.about_us') }}</li>
                             </ul>
